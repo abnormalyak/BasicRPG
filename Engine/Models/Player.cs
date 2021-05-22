494ae086010aabc97f5169ad3b380ae25aa731de
Wing Ng <tockets.alt@gmail.com>
2021-05-22 11:58:49 +0100

    Added Player class: represents a player.
    Added GameSession class: represents the current game session.

---
+++ Engine/Models/Player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Engine.Models
+{
+    public class Player
+    {
+        public string Name { get; set; }
+        public string Class { get; set; }
+        public int Health { get; set; }
+        public int Experience { get; set; }
+        public int Level { get; set; }
+        public int Gold { get; set; }
+    }
+}
---
+++ Engine/ViewModels/GameSession.cs
@@ -0,0 +1,19 @@
+﻿using Engine.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Engine.ViewModels
+{
+    class GameSession
+    {
+        Player CurrentPlayer { get; set; }
+
+        public GameSession()
+        {
+            CurrentPlayer = new Player();
+            CurrentPlayer.Name = "Abnormal";
+            CurrentPlayer.Gold = 500;
+        }
+    }
+}
