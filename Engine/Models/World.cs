ccd30efbcd68a129a2a0f9e166146c4891749c8e
Wing Ng <tockets.alt@gmail.com>
2021-06-03 16:22:01 +0100

    Created the World class, which holds a list of locations, defining that world.
    Created the AddLocation method for creating and adding a location to the world.
    
    Created the LocationAt method, which returns a Location at the given coordinates.

---
+++ Engine/Models/World.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Engine.Models
+{
+    public class World
+    {
+        private List<Location> _locations = new List<Location>();
+
+        internal void AddLocation(int xCoord, int yCoord, string name,
+            string description, string imgName)
+        {
+            Location loc = new Location();
+            loc.XCoordinate = xCoord;
+            loc.YCoordinate = yCoord;
+            loc.Name = name;
+            loc.Description = description;
+            loc.ImageName = imgName;
+
+            _locations.Add(loc);
+        }
+
+        public Location LocationAt(int xCoord, int yCoord)
+        {
+            foreach (Location l in _locations)
+            {
+                if (l.XCoordinate == xCoord && l.YCoordinate == yCoord)
+                {
+                    return l;
+                }
+            }
+
+            return null;
+        }
+    }
+}
