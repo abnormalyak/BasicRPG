c102564f6550b71b4f2baca07648f5f255f0faaf
Wing Ng <tockets.alt@gmail.com>
2021-06-03 16:22:36 +0100

    Created the WorldFactory class, with a method to create a world object and fill it with locations.

---
+++ Engine/Factories/WorldFactory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Engine.Models;
+
+namespace Engine.Factories
+{
+    internal class WorldFactory
+    {
+        internal World CreateWorld()
+        {
+            World newWorld = new World();
+            
+
+            newWorld.AddLocation(-2, -1, "Overgrown Field",
+                "A dazingly beige field of corn, overrun with bees.",
+                "pack://application:,,,/Engine;component/Images/Locations/BeeField.png");
+
+            newWorld.AddLocation(-1, -1, "Farmhouse",
+                "An out-of-the-way farmhouse - is anyone inside?",
+                "pack://application:,,,/Engine;component/Images/Locations/Farmhouse.png");
+
+            newWorld.AddLocation(0, -1, "Home", "Your house.",
+            "pack://application:,,,/Engine;component/Images/Locations/Home.png");
+            return newWorld;
+        }
+    }
+}
