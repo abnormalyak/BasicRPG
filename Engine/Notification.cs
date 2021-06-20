d7a4f15573c78061fc689203bc77432f57f0a3da
Wing Ng <tockets.alt@gmail.com>
2021-06-20 17:20:27 +0100

    Mistake in previous commit; hadn't saved file.

--- Engine/Notification.cs
+++ Engine/Notification.cs
@@ -1,10 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 
 namespace Engine
 {
-    class Notification
+    public class Notification : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string property)
+        {
+            // ? after a variable performs a null check before calling the following method
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+        }
     }
 }
