964f7ee1721a702d4dc590e528ba60aec1659de4
Wing Ng <tockets.alt@gmail.com>
2021-06-30 10:43:47 +0100

    Created QuestStatus class, used to show whether the player has completed a given quest. Added an ObservableCollection of QuestStatus as an attribute to the Player class to track the Player's quests.

---
+++ Engine/Models/QuestStatus.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Engine.Models
+{
+    public class QuestStatus
+    {
+        public Quest Quest { get; set; }
+        public bool IsComplete { get; set; }
+
+        public QuestStatus(Quest quest)
+        {
+            Quest = quest;
+            IsComplete = false;
+        }
+    }
+}
