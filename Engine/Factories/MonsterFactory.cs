fbfcc1217b1bcce1561282ec39a48153e422e564
Wing Ng <tockets.alt@gmail.com>
2021-06-30 12:06:42 +0100

    Created the MonsterFactory class, used to generate hardcoded Monster objects by ID. Created the Bee monster, added an original image for the Bee (Bee.png). Updated resource handling in .csproj (changed to Do Not Copy).

---
+++ Engine/Factories/MonsterFactory.cs
@@ -0,0 +1,36 @@
+﻿using Engine.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Engine.Factories
+{
+    public static class MonsterFactory
+    {
+        public static Monster GetMonster(int id)
+        {
+            switch (id)
+            {
+                case 1:
+                    Monster bee = new Monster("Bee", "Bee.png", 10, 10, 20, 3);
+
+                    AddLootItem(bee, 9001, 40);
+                    AddLootItem(bee, 9002, 60);
+
+                    return bee;
+                default:
+                    throw new ArgumentException(
+                        string.Format("Monster with ID '{0}' does not exist.", id));
+            }
+        }  
+        private static void AddLootItem(Monster monster, int itemID, int dropChance)
+        {
+            if (RandomNumberGenerator.NumberBetween(1, 100) <= dropChance)
+            {
+                monster.Inventory.Add(new ItemQuantity(itemID, 1));
+            }
+        }
+    }
+
+  
+}
