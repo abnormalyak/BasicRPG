3b4c715998e5c6c7cebec225b2d7e7890c6aead0
DESKTOP-45N88FN\tocke <tockets.alt@gmail.com>
2021-07-05 16:03:04 +0100

    Added new property to Player, representing weapons carried by the player.

--- Engine/Models/Player.cs
+++ Engine/Models/Player.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.ComponentModel;
 using System.Collections.ObjectModel;
+using System.Linq;
 
 namespace Engine.Models
 {
@@ -66,12 +67,22 @@ namespace Engine.Models
 
         public ObservableCollection<GameItem> Inventory { get; set; }
 
+        public List<GameItem> Weapons => 
+            Inventory.Where(i => i is Weapon).ToList();
+
         public ObservableCollection<QuestStatus> Quests { get; set; }
 
         public Player()
         {
             Inventory = new ObservableCollection<GameItem>();
             Quests = new ObservableCollection<QuestStatus>();
+        }
+
+        public void AddItemToInventory(GameItem item)
+        {
+            Inventory.Add(item);
+
+            OnPropertyChanged(nameof(Weapons));
         }
     }
 }
