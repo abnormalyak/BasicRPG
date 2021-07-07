3d4540334350e276d3ea35e90a589e86a28e8713
DESKTOP-45N88FN\tocke <tockets.alt@gmail.com>
2021-07-07 20:51:18 +0100

    Created new class representing a trader.

---
+++ Engine/Models/Trader.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace Engine.Models
+{
+    public class Trader : Notification
+    {
+        public string Name { get; set; }
+
+        public ObservableCollection<GameItem> Inventory { get; set; }
+
+        public Trader(string name)
+        {
+            Name = name;
+            Inventory = new ObservableCollection<GameItem>();
+        }
+
+        public void AddItemToInventory(GameItem item)
+        {
+            Inventory.Add(item);
+        }
+
+        public void RemoveItemFromInventory(GameItem item)
+        {
+            Inventory.Remove(item);
+        }
+    }
+}
