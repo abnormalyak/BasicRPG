ba92b9d528fe8bf8943bf075316422ec5995617a
DESKTOP-45N88FN\tocke <tockets.alt@gmail.com>
2021-07-10 20:26:55 +0100

    Added IsUnique property to GameItem.

--- Engine/Models/GameItem.cs
+++ Engine/Models/GameItem.cs
@@ -9,17 +9,19 @@ namespace Engine.Models
         public int ItemID { get; set; }
         public string Name { get; set; }
         public int Value { get; set; }
+        public bool IsUnique { get; set; }
 
-        public GameItem(int id, string name, int value)
+        public GameItem(int id, string name, int value, bool isUnique = false)
         {
             ItemID = id;
             Name = name;
             Value = value;
+            IsUnique = isUnique;
         }
 
         public GameItem Clone()
         {
-            return new GameItem(ItemID, Name, Value);
+            return new GameItem(ItemID, Name, Value, IsUnique);
         }
     }
 }
